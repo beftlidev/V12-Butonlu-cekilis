f8066d16a100c13f42644c910d26a26436f3b216
iUgur <77486662+iUgur@users.noreply.github.com>
2021-06-20 19:41:52 +0300

    Update yeniden-çek.js

--- komutlar/yeniden-çek.js
+++ komutlar/yeniden-çek.js
@@ -1,6 +1,10 @@
+const Discord = require('discord.js');
+const data = require('croxydb')
 const Nuggies = require('nuggies');
 const  { MessageButton } = require('discord-buttons');
 module.exports.run = async (client, message, args) => {
+  let açıkmı = await data.fetch(`pre_${message.author.id}`)
+  if(açıkmı) {
     if (!args[0]) return message.reply('Lütfen bir mesaj kimliği verin!', { allowedMentions: { repliedUser: false } });
     let win;
     try {
@@ -12,6 +16,12 @@ module.exports.run = async (client, message, args) => {
     }
     if (!win[0]) return message.channel.send('There are not enough people in the giveaway!');
     message.channel.send(`Yeniden çekildi! <@${win}> hediyenin yeni kazananı! `, { component: new MessageButton().setLabel('Giveaway').setURL(`https://discord.com/channels/${message.guild.id}/${message.channel.id}/${args[0]}`).setStyle('url') });
+} else { return message.channel.send(new Discord.MessageEmbed()
+ .setDescription(`Bu komut premiumlulara özel!
+Premium almak için [Destek sunucumuzu](https://discord.gg/KZfAEjrPUF) ziyaret et!`)
+.setTimestamp()
+)
+} 
 }
 
 exports.help = {
